c7b7178a46d706496ec39d71e2ba54bac946fa5b
Milena Ivanova <milena_ii@abv.bg>
2017-03-06 13:43:10 +0200

    Lectures_ multidimensional arrays

---
+++ MultidimentionalArrays/6.MethodsOfArrays2/MethodOfArrays2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class MethodOfArrays2
+{
+    static void Main()
+    {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+
+        // Copy Array
+        int[] copyArray = new int[7];
+
+        Array.Copy(nums, copyArray, 7);
+        Console.WriteLine("copy     " + string.Join(", ",copyArray));
+
+        //Reverse Array
+
+        Array.Reverse(nums);
+        Console.WriteLine("reverse    " + string.Join(", ",nums));
+
+        // Sort
+
+        int[] unsortArr = { 4, 3, 4, 9, 5, 0, 2 };
+
+        Array.Sort(unsortArr);
+        Console.WriteLine("sort    " + string.Join(", ",unsortArr));
+
+
+        Array.Reverse(unsortArr);
+        Console.WriteLine("sort reverse   " + string.Join(", ", unsortArr));
+
+
+        // Sort Array of strings - lexicographical
+
+        string[] alphabet = { "A", "C", "Gb", "D", "F", "B", "E", "Ga", "Gc" };
+        Array.Sort(alphabet);
+        Console.WriteLine("sort strings   " + string.Join(", ", alphabet));
+
+        Console.WriteLine();
+    }
+}
+
---
+++ MultidimentionalArrays/MethodsOfArray/MethodOfArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class MethodOfArray
+
+{
+    static void Main()
+    {
+        int[] nums = { 5, 6, 3, 7, 2, 1, 5, 6, 4 };
+
+        Console.WriteLine(Array.IndexOf(nums,6)); //1
+
+        Console.WriteLine(Array.LastIndexOf(nums,6)); //7
+
+        // Lets make IndexOf by ourselves:
+
+        int searchNum = 6;
+
+        int index;
+        for (index = 0; index < nums.Length ; index++)
+        {
+            if (nums[index] == searchNum)
+            {
+                break;
+            }
+        }
+        Console.WriteLine(index);
+
+        // Lets make IndexOf by ourselves:
+
+        int searchN = 6;
+
+        int ind;
+        for (ind = nums.Length - 1; ind >= 0; ind--)
+        {
+            if (nums[ind] == searchN)
+            {
+                break;
+            }
+        }
+        Console.WriteLine(ind);
+
+
+    }
+}
+
