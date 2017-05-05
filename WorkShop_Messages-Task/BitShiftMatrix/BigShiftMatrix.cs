9d0f4690c71b8ddc2aa3c6d98ecae09b5d8a6cee
Milena Ivanova <milena_ii@abv.bg>
2017-05-05 15:19:13 +0300

    Workshop two tasks

---
+++ WorkShop_Messages-Task/BitShiftMatrix/BigShiftMatrix.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace BitShiftMatrix
+{
+    class BigShiftMatrix
+    {
+        static int rows;    // за да ги ползваме във вс методи
+        static int cols;
+        static int[] moves;
+        static BigInteger[,] field;
+
+        static void Main()
+        {
+            //read the input 
+            rows = int.Parse(Console.ReadLine());
+            cols = int.Parse(Console.ReadLine());
+            int movesCount = int.Parse(Console.ReadLine());
+            moves = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            //second way
+            //string[] moves2 = Console.ReadLine().Split(' ');
+
+            //int[] arr = new int[moves2.Length];
+
+            //for (int i = 0; i < moves2.Length; i++)
+            //{
+            //    arr[i] = int.Parse(moves2[i]);
+            ////}
+
+            field = new BigInteger[rows, cols];
+            FillMatrix();  // може да не му подаваме матрицата 
+                           //т.к. отгоре static си я четем
+
+            BigInteger sum = 0;
+            //Как да пазим позицията на която сме - или масив с две променливи
+            int[] pos = { rows - 1, 0 };
+            // или с две отделни променливи
+            //int currentRow;
+            //int currentCol;
+
+            int coef = rows > cols ? rows : cols;
+            //int coef2 = Math.Max(rows, cols);
+
+            //Трябва да изчислим за всяко движение до кой ред и колона ще стигнем
+            // т. е. обхождане на moves: foreach
+
+            sum += field[pos[0], pos[1]]; // ако съм на 0,0 и искам да отида на 0,0 - тр да се сетим сами
+            field[pos[0], pos[1]] = 0;  // защото не искаме да се повтарят - имаме 0, като я събере ние сме ок 
+
+            foreach (var code in moves)
+            {
+                int[] tarPos = { code / coef, code % coef };
+                //обхождайки тр да сумираме цифрите,к са там:
+
+                int stepRow = pos[0] == tarPos[0] ? 0 : (pos[0] < tarPos[0] ? 1 : -1 );  // ако тр да отида наляво ред -1 или 1
+                int stepCol = pos[1] == tarPos[1] ? 0 : (pos[1] < tarPos[1] ? 1 : -1);  // аналог кол, равното не го ползваме
+                // 2 way
+               // int stepRow = Math.Sign(tarPos[0] - pos[0]);
+               // int stepCol = Math.Sign(tarPos[1] - pos[1]); 
+
+                // Math.Sign(a - b) ->  1 -> a > b,
+                //                  ->  0 -> a = b,
+                //                  -> -1 -> a < b
+                do
+                {
+                    pos[1] += stepCol;
+                    sum += field[pos[0], pos[1]];
+                    field[pos[0], pos[1]] = 0;
+                    
+                } while (pos[1] != tarPos[1]);  // докато поз на кол е != от таргет кол
+                //while (pos[1] != tarPos[1] && pos[1] >= 0 && pos[1] < cols) + гаранц,
+                // че сме в матрицата 
+                do
+                {
+                    pos[0] += stepRow;
+                    sum += field[pos[0], pos[1]];
+                    field[pos[0], pos[1]] = 0;
+                    
+                } while (pos[0] != tarPos[0]);
+
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static void PrintMatrix(BigInteger[,] board)
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    Console.Write("{0,4}", board[r, c]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillMatrix()
+        {
+            for (int r = 0; r < rows; r++)  // 2 way with Field.GetLength
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    field[r, c] = (BigInteger)(1) << (rows - 1 - r + c); // 2 на степен нещо - побитово отместване
+                }
+            }
+        }
+    }
+}
---
+++ WorkShop_Messages-Task/WorkShop_Messages-Task/Workshop.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Numerics;
+//Joro and Gosho are very good friends. They love to talk and devise master
+//plans for world domination. Yet, John the Evil wants to steal their ideas.
+//So, they have built an encryption system for communication.
+//The encryption system consists of adding or subtracting numbers in their
+//GeorgeTheGreat numeral system.The GeorgeTheGreat numeral system has exactly 
+//10 different digits and each digit consists of 3 chacters as follows:....
+
+//calculate the result of the operation, by given two numbers in GeorgeTheGreat 
+//numeral system and an operator (subtraction or addition)
+
+namespace WorkShop_Messages_Task
+{
+    class Workshop
+    {
+        static string[] numSystem = { "cad", "xoz", "nop", "cyk", "min", "mar", "kon", "iva", "ogi", "yan" };
+        //field, в класа, за да може да се използва във всеки един метод без да тр 
+        // да го прехвърлям като параметър
+
+        static void Main()
+        {
+            //reading input
+            string strOne = Console.ReadLine();
+            string op = Console.ReadLine();
+            string strTwo = Console.ReadLine();
+
+            //methods
+            BigInteger numOne = Decrypt(strOne);
+            BigInteger numTwo = Decrypt(strTwo);
+
+            BigInteger numResult = (op == "+" ? numOne + numTwo : numOne - numTwo); 
+            //sum or substract
+
+            string result = Encrypt(numResult);
+            Console.WriteLine(result);
+        }
+
+        private static BigInteger Decrypt(string str) // mar kon cyk yan ogi 
+        {
+            BigInteger result = 0;
+
+            string digit; // като го инициализираме тук, ще имаме една 
+                          // променлива, която ще преизползваме всеки път в цикъла
+            for (int j = 0; j < str.Length; j += 3)
+            {
+                digit = str.Substring(j, 3); // string digit... ако иниц тук, вс път
+                // при въртене ще иниц нова променлива - малко по-бавно
+                for (int i = 0; i < numSystem.Length; i++)
+                {
+                    if (digit == numSystem[i])
+                    {
+                        result = result * 10 + i;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static string Encrypt(BigInteger num)
+        {
+            string result = "";
+
+            int digit = 0;
+            while (num > 0)
+            {
+                digit = (int)(num % 10);
+                result = numSystem[digit] ;
+                num /= 10;
+            }
+
+            return result;
+        }
+
+    }
+}
